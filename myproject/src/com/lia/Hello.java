ca3c65cd5a54dd9d1c5e14e73bdf9c6f74e8ca9c
user <user@user>
2018-10-11 22:16:43 +0800

    10/11 practice

---
+++ myproject/src/com/lia/Hello.java
@@ -0,0 +1,15 @@
+package com.lia;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person(80,70);
+		p.english = 80;
+		p.math = 70;
+		System.out.println(p.english);
+		System.out.println(p.math);
+		 
+	}
+
+}
---
+++ myproject/src/com/lia/Person.java
@@ -0,0 +1,14 @@
+package com.lia;
+
+public class Person {
+	float english;
+	float math;
+	
+	public Person(float english,float math) {
+		this.english = english;
+		this.math = math;
+		//
+
+	}
+
+}
